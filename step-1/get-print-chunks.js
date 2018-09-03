db7fec81b0ca2e26b1fe3b5f4032a95e0fd28ffe
Aaron Parsons <aaron.parsons@live.ca>
2018-09-03 15:30:45 +0000

    completed step 1

--- step-1/get-print-chunks.js
+++ step-1/get-print-chunks.js
@@ -7,8 +7,19 @@ function getAndPrintHTMLChunks() {
     path: '/http-examples/step1.html'
   };
 
+  // Send the request
   https.get(requestOptions, function(response) {
 
+    // Set encoding to UTF-8
+    response.setEncoding('utf8');
+
+    // WHen a chunk of data is received
+    response.on('data', function(data) {
+      console.log('--- Chunk received. Length: ' + data.length + '---');
+      console.log(data + '\n');
+    });
   });
 
 }
+
+getAndPrintHTMLChunks();
