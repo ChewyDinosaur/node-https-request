9f0078d3a4ffcb5c381ecde5b6298cbaa04e1672
Aaron Parsons <aaron.parsons@live.ca>
2018-09-03 16:10:05 +0000

    completed step 5, exported request function as a module

---
+++ step-5/http-function.js
@@ -0,0 +1,25 @@
+const https = require('https');
+
+module.exports = function getHTML (options, callback) {
+  let output = '';
+
+  // Send the request
+  https.get(options, function(response) {
+
+    // Set encoding to UTF-8
+    response.setEncoding('utf8');
+
+    // When a chunk of data is received, append it to the output
+    response.on('data', function(data) {
+      console.log('--- Chunk received. Length: ' + data.length + '---');
+      output += data;
+    });
+    
+    // When all data has been received, log it to the console
+    response.on('end', function() {
+      console.log('Request complete, logging the data: ')
+      callback(output);
+    });
+  });
+}
+
